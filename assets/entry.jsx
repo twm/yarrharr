ef985a77991a0e9b3199019449bc9462900fb1dd
Tom Most <twm@freecog.net>
2015-12-12 16:43:27 -0800

    Graft the old app into redux

--- assets/entry.jsx
+++ assets/entry.jsx
@@ -1,5 +1,31 @@
-// This file is the Webpack entry point to the whole codebase.  It exports
-// names to the global scope.
-window.React = require("react");
-window.ReactDOM = require("react-dom");
-window.Yarrharr = require("./Yarrharr.jsx");
+// This file is the Webpack entry point to the whole codebase.
+import { createStore, combineReducers } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { Router, Route } from 'react-router';
+import { createHistory } from 'history';
+import { syncReduxAndRouter, routeReducer } from 'redux-simple-router';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Yarrharr from "./Yarrharr.jsx";
+
+
+const reducer = combineReducers({
+    // FIXME: Replace this with routing and proper abstractions.
+    yarrharrProps: (state = window.props, action) => state,
+    routing: routeReducer,
+});
+const store = createStore(reducer);
+const history = createHistory();
+
+syncReduxAndRouter(history, store);
+
+const YarrharrRedux = connect(state => state.yarrharrProps, null)(Yarrharr);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router history={history}>
+            <Route path="/" component={YarrharrRedux} />
+        </Router>
+    </Provider>,
+    document.getElementById("app")
+);
