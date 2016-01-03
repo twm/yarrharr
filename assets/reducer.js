660fb5544c068599afff9cfd4325971815eb739e
Tom Most <twm@freecog.net>
2016-01-02 22:40:25 -0800

    Move reducers to a separate file

---
+++ assets/reducer.js
@@ -0,0 +1,111 @@
+import { combineReducers } from 'redux';
+import { routeReducer } from 'redux-simple-router';
+
+import { SET_VIEW, VIEW_LIST, SET_FILTER, FILTER_NEW, SET_ORDER, ORDER_DATE } from './actions.js';
+
+import { REQUEST_ARTICLES, RECEIVE_ARTICLES, FAIL_ARTICLES } from './actions.js';
+import { REQUEST_MARK_ARTICLE, RECEIVE_MARK_ARTICLE, FAIL_MARK_ARTICLE } from './actions.js';
+
+function articleReducer(state = window.props.articlesById, action) {
+    if (action.type === REQUEST_ARTICLES) {
+        const patch = {};
+        const flags = {loading: true, error: false};
+        action.articleIds.forEach((id) => {
+            patch[id] = Object.assign({}, state[id], flags);
+        });
+        return Object.assign({}, state, patch);
+    } else if (action.type === RECEIVE_ARTICLES) {
+        return Object.assign({}, state, action.articlesById);
+    } else if (action.type === FAIL_ARTICLES) {
+        const patch = {};
+        const flags = {loading: false, error: true};
+        action.articleIds.forEach((id) => {
+            patch[id] = Object.assign({}, state[id], flags);
+        });
+        return Object.assign({}, state, patch);
+    } else if (action.type === REQUEST_MARK_ARTICLE) {
+        const article = state[action.articleId];
+        return Object.assign({}, state, {
+            [action.articleId]: Object.assign({}, article, {marking: action.state}),
+        });
+    } else if (action.type === RECEIVE_MARK_ARTICLE) {
+        const article = state[action.articleId];
+        return Object.assign({}, state, {
+            [action.articleId]: Object.assign({}, article, {
+                state: action.state,
+                marking: null,
+            }),
+        });
+    } else if (action.type === FAIL_MARK_ARTICLE) {
+        // TODO: Communicate the error to the user somehow.
+        const article = state[action.articleId];
+        return Object.assign({}, state, {
+            [action.articleId]: Object.assign({}, article, {marking: null}),
+        });
+    }
+    return state;
+}
+
+function feedReducer(state = window.props.feedsById, action) {
+    // TODO: Support feed CRUD.
+    return state;
+}
+
+import { REQUEST_SNAPSHOT, RECEIVE_SNAPSHOT, FAIL_SNAPSHOT } from './actions.js';
+const defaultSnapshot = {
+    loading: false,
+    error: false,
+    order: ORDER_DATE,
+    filter: FILTER_NEW,
+    feedIds: [],
+    articleIds: [],
+};
+function snapshotReducer(state = defaultSnapshot, action) {
+    if (action.type === REQUEST_SNAPSHOT) {
+        return Object.assign({}, state, {
+            loading: true,
+            error: false,
+            order: action.order,
+            filter: action.filter,
+            feedIds: action.feedIds,
+            articleIds: [],
+        });
+    } else if (action.type === RECEIVE_SNAPSHOT) {
+        return Object.assign({}, state, {
+            loading: false,
+            error: false,
+            order: action.order,
+            filter: action.filter,
+            feedIds: action.feedIds,
+            articleIds: action.articleIds,
+        });
+    } else if (action.type === FAIL_SNAPSHOT) {
+        return Object.assign({}, state, {
+            loading: false,
+            error: true,
+        });
+    }
+    return state;
+}
+
+function labelReducer(state = window.props.labelsById, action) {
+    // TODO: Support label CRUD.
+    return state;
+}
+
+function viewReducer(state = VIEW_LIST, action) {
+    if (action.type === SET_VIEW) {
+        return action.view;
+    }
+    return state;
+}
+
+
+module.exports = combineReducers({
+    articlesById: articleReducer,
+    feedsById: feedReducer,
+    snapshot: snapshotReducer,
+    labelsById: labelReducer,
+    view: viewReducer,
+    routing: routeReducer,
+});
