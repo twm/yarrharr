458fd800cee4f9ab8ddf3d5364f35befaba902f8
Tom Most <twm@freecog.net>
2016-04-17 15:07:45 -0700

    ArticleView: Add StateToggle, title in toolbar
    This is quite crude yet.

--- assets/views/ArticleView.js
+++ assets/views/ArticleView.js
@@ -5,31 +5,48 @@ import Loading from 'widgets/Loading.js';
 import { Logo } from 'widgets/icons.js';
 import { RootLink, FeedLink } from 'widgets/links.js';
 import { markArticle } from 'actions.js';
+import StateToggle from 'widgets/StateToggle.js';
 import './ArticleView.less';
 
 function ArticleView(props) {
     const { dispatch, articlesById, params: { articleId }, feedsById } = props;
     const article = articlesById[articleId];
+    const feed = feedsById[article.feedId];
     // FIXME: This shouldn't hardcode view-wide
     return <div className="article-view view-wide">
         <div className="controls">
             <RootLink className="toolbar-button" title="Return to feed list">
                 <Logo />
             </RootLink>
+            {renderArticleSummary(article, feed)}
+            {!article || article.loading ?  null :
+                <StateToggle
+                    onMark={(articleId, targetState) => dispatch(markArticle(articleId, targetState))}
+                    {...article} />}
         </div>
-        {renderArticle(article)}
+        {renderArticle(article, feed)}
     </div>;
 }
 
-function renderArticle(article) {
+function renderArticle(article, feed) {
     if (!article || article.loading) {
         return <div className="placeholder"><Loading /></div>;
     }
     return <Article
-        feed={props.feedsById[article.feedId]}
+        feed={feed}
         onMark={(articleId, targetState) => dispatch(markArticle(articleId, targetState))}
         {...article} />;
-k}
+}
+
+function renderArticleSummary(article, feed) {
+    if (!article.id || article.loading) {
+        return <div className="article-toolbar-summary" />
+    }
+    return <div className="article-toolbar-summary">
+        {article.title}<br />From {feed.text || feed.title}
+    </div>;
+}
+
 ArticleView.propTypes = {
     params: React.PropTypes.shape({
         articleId: React.PropTypes.string.isRequired,
