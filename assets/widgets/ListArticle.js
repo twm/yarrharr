fe418058dbf323c7981a5f954dd4e68c613e1e33
Tom Most <twm@freecog.net>
2016-04-27 21:41:19 -0700

    Clean up ListView
    * Rearrange widgets and restyle to match the article view.  Now the
      state toggle and external link are in the margins.
    * Remove links to feeds so that the main click target is larger.  This
    * makes the list view better-suited to touch interaction (for desktops,
      a table view will be added which preserves this information).

--- assets/widgets/ListArticle.js
+++ assets/widgets/ListArticle.js
@@ -1,50 +1,20 @@
 import React from 'react';
 import { ArticleLink, FeedLink } from 'widgets/links.js';
-import { ArrowRight } from 'widgets/icons.js';
+import { Outbound } from 'widgets/icons.js';
 import StateToggle from 'widgets/StateToggle.js';
 import "./ListArticle.less";
 
-const MONTH_TO_WORD = {
-    '01': 'Jan',
-    '02': 'Feb',
-    '03': 'Mar',
-    '04': 'Apr',
-    '05': 'May',
-    '06': 'Jun',
-    '07': 'Jul',
-    '08': 'Aug',
-    '09': 'Sep',
-    '10': 'Oct',
-    '11': 'Nov',
-    '12': 'Dec',
-};
-
-
-function VerticalDate({dateTime}) {
-    const year = dateTime.slice(0, 4);
-    const month = dateTime.slice(5, 7);
-    const day = dateTime.slice(8, 10);
-    const monthWord = MONTH_TO_WORD[month];
-    return <time className="date-block" dateTime={dateTime}>
-        <span className="month">{monthWord}</span>
-        <span className="day">{day.replace(/^0/, '')}</span>
-        <span className="year">{year}</span>
-    </time>;
-}
-
 function ListArticle(props) {
     return <div className="list-article">
-        <StateToggle {...props} />
-        <div className="middle">
-            <FeedLink feedId={props.feed.id} className="feed">
-                {props.feed.text || props.feed.title}
-            </FeedLink>
-            <a className="external-link" href={props.url} target="_blank">{props.title}</a>
-        </div>
+        <StateToggle className="button" {...props} />
         <ArticleLink articleId={props.id} className="view-link">
-            <VerticalDate dateTime={props.date} />
-            <ArrowRight alt="View Article" className="arrow" />
+            <span className="meta">{props.feed.text || props.feed.title} on {props.date}</span>
+            <br />
+            {props.title}
         </ArticleLink>
+        <a href={props.url} className="button" target="_blank" title="View on source site">
+            <Outbound alt="View on source site" width="32" height="32" />
+        </a>
     </div>;
 }
 
