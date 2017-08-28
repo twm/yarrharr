57cb4cc7dfd8d77a5b7643e96b905180735c6ffb
Tom Most <twm@freecog.net>
2017-08-27 19:12:20 -0700

    Cope with webpack --watch behavior
    It doesn't clean the static file directory, so including main.*.js is
    including many files!

--- yarrharr/templatetags/static_glob.py
+++ yarrharr/templatetags/static_glob.py
@@ -28,8 +28,10 @@ A template filter which matches against the yarrharr app's static files.
 """
 
 import fnmatch
+import stat
 import os
 
+import attr
 from django import template
 from django.template.defaultfilters import stringfilter
 
@@ -38,14 +40,47 @@ register = template.Library()
 _static_dir = os.path.join(os.path.dirname(__file__), '../static')
 
 
+@attr.s
+class NoStaticMatch(Exception):
+    pattern = attr.ib()
+
+    def __str__(self):
+        return "No file matches pattern {!r}".format(self.pattern)
+
+
 @register.filter
 @stringfilter
-def static_glob(pattern):
+def newest_static(pattern):
     """
-    Match a glob pattern against the files in yarrharr's static file directory.
+    Search the Yarrharr app's static directory for the newest file matching the
+    given fnmatch pattern.
+
+    This combines with a Webpack output file pattern that incorporates a hash
+    of the file contents to ensure that the browser always receives the latest
+    file without sending cache-busting headers. Discarding all but the newest
+    files is necessary because Webpack's watch mode doesn't clear files from
+    the output directory (``CleanWebpackPlugin`` doesn't operate).
+
+    :param str pattern: fnmatch (glob) pattern -- see :mod:`fnmatch`
+    :returns: name of the file with the greatest modification time in the directory
+    :raises: :exc:`NoStaticMatch` when no file matching the pattern exists
     """
     # TODO The result of this should be cached when not in DEBUG mode.
     assert '/' not in pattern  # don't support subdirectories
-    paths = os.listdir(_static_dir)
-    return [fn for fn in paths if fnmatch.fnmatchcase(fn, pattern) and
-            os.path.isfile(os.path.join(_static_dir, fn))]
+    name, mtime = None, None
+    for fn in os.listdir(_static_dir):  # *sigh* need Python 3 for scandir...
+        if not fnmatch.fnmatchcase(fn, pattern):
+            continue
+
+        s = os.stat(os.path.join(_static_dir, fn))
+        if not stat.S_ISREG(s.st_mode):
+            continue
+
+        if mtime is None or s.st_mtime > mtime:
+            name = fn
+            mtime = s.st_mtime
+
+    if name is None:
+        raise NoStaticMatch(pattern)
+
+    return name
