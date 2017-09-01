71b14f749ea65709dabf7ee425d0bf27e5d0d396
Tom Most <twm@freecog.net>
2017-09-01 11:26:30 -0700

    Somewhat less expensive placeholders

--- assets/widgets/ListArticle.js
+++ assets/widgets/ListArticle.js
@@ -4,21 +4,13 @@ import { Outbound } from 'widgets/icons.js';
 import { ReadToggle } from 'widgets/StateToggle.js';
 import "./ListArticle.less";
 
+const PLACEHOLDER = <div className="list-article" />;
+const BR = <br />;
+const OUTBOUND_ICON = <Outbound alt="View on source site" width="32" height="32" />;
+
 export default function ListArticle(props) {
     if (props.loading) {
-        return <div className="list-article">
-            <span className="button"> </span>
-            {props.renderLink({
-                articleId: props.id,
-                className: "view-link",
-                children: [
-                    <span className="meta">...</span>,
-                    <br />,
-                    "...",
-                ],
-            })}
-            <span className="button"> </span>
-        </div>;
+        return PLACEHOLDER;
     }
     return <div className="list-article">
         <ReadToggle articleId={props.id} read={props.read} onMarkArticlesRead={props.onMarkArticlesRead} />
@@ -27,12 +19,12 @@ export default function ListArticle(props) {
             className: "view-link",
             children: [
                 <span className="meta">{props.feed.text || props.feed.title} on {props.date}</span>,
-                <br />,
+                BR,
                 props.title || "Untitled",
             ],
         })}
         <a href={props.url} className="button" target="_blank" title="View on source site">
-            <Outbound alt="View on source site" width="32" height="32" />
+            {OUTBOUND_ICON}
         </a>
     </div>;
 }
