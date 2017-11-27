39bfef55ace5645299d6f481cbab9f6918bca536
Tom Most <twm@freecog.net>
2017-11-26 23:18:00 -0800

    Remove unnecessary x and w attributes

--- assets/widgets/icons.js
+++ assets/widgets/icons.js
@@ -29,7 +29,7 @@ import LabelIcon from '../icons/label.svg';
 export { LabelIcon as LabelIcon };
 
 export function FeedIcon(props) {
-    return <svg x="0" y="0" width="1em" height="1em" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" {...props}>
+    return <svg width="1em" height="1em" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" {...props}>
         <circle cx="3.5" cy="16.5" r="2.5" fill="none" stroke="#666" stroke-width="2" />
         <path d="M 1 8 A 11 11, 0, 0, 1, 12 19" fill="none" stroke="#666" stroke-width="2" stroke-linejoin="round" stroke-linecap="round" />
         <path d="M 1 2 A 17 17, 0, 0, 1, 18 19" fill="none" stroke="#666" stroke-width="2" stroke-linejoin="round" stroke-linecap="round" />
@@ -37,7 +37,7 @@ export function FeedIcon(props) {
 }
 
 export function EditIcon(props) {
-    return <svg x="0" y="0" width="1em" height="1em" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" {...props}>
+    return <svg width="1em" height="1em" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" {...props}>
         <path d="M 15 1 A 2 2, 0, 0, 1, 18 4 L 17 5 L 14 2 Z" fill="currentColor" stroke="none" stroke-width="2" stroke-linejoin="round" stroke-linecap="round" />
         <path d="M 13 3 L 16 6 L 6 16 L 2 17 L 3 13 Z" fill="currentColor" stroke="none" stroke-width="2" stroke-linejoin="round" stroke-linecap="round" />
         <path d="M 1 19 L 19 19" fill="none" stroke="currentColor" stroke-width="2" stroke-linejoin="round" stroke-linecap="round" />
