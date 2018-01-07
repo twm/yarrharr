a28acaf616c178aa5a4e53bb9e2bccdb329da6ac
Tom Most <twm@freecog.net>
2018-01-07 15:17:56 -0800

    Move class into .svg files

--- assets/widgets/GlobalBar.js
+++ assets/widgets/GlobalBar.js
@@ -25,7 +25,9 @@ export class LayoutToggleLink extends React.PureComponent {
         const Image = narrow ? WideIcon : NarrowIcon;
         const text = narrow ? "Wide" : "Narrow"
         const actionText = narrow ? "Switch to wide layout" : "Switch to narrow layout"
-        return <a role="button" className="square" tabIndex="0" aria-label={text} href="#" onClick={this.handleClick} title={actionText} ><Image className="icon" aria-hidden={true} /></a>;
+        return <a role="button" className="square" tabIndex="0" aria-label={text} href="#" onClick={this.handleClick} title={actionText} >
+            <Image aria-hidden={true} />
+        </a>;
     }
 }
 
