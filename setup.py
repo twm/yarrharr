af3165fe6bacd1fa6145602811a90e0ddc8c0204
Tom Most <twm@freecog.net>
2018-01-17 22:38:33 -0800

    Anoint 0.7.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yarrharr',
-    version='0.6.0',
+    version='0.7.0',
     url='https://github.com/twm/yarrharr',
     author='Tom Most',
     author_email='yarrharr@freecog.net',
