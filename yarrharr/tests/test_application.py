a9130b6b688d3904df3f596588b92df1fb4084ba
Tom Most <twm@freecog.net>
2018-02-03 20:31:23 -0800

    Rough out systemd log integration

---
+++ yarrharr/tests/test_application.py
@@ -0,0 +1,45 @@
+# Copyright © 2018 Tom Most <twm@freecog.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Additional permission under GNU GPL version 3 section 7
+#
+# If you modify this Program, or any covered work, by linking or
+# combining it with OpenSSL (or a modified version of that library),
+# containing parts covered by the terms of the OpenSSL License, the
+# licensors of this Program grant you additional permission to convey
+# the resulting work.  Corresponding Source for a non-source form of
+# such a combination shall include the source code for the parts of
+# OpenSSL used as well as that of the covered work.
+
+from treq.client import HTTPClient
+from treq.testing import RequestTraversalAgent
+from twisted.trial.unittest import TestCase
+from twisted.internet import defer
+
+from ..application import Root
+
+
+class RootTests(TestCase):
+    @defer.inlineCallbacks
+    def _test_static_resources(self):
+        """
+        All static resources linked from the login page load.
+        """
+        from twisted.internet import reactor
+
+        treq = HTTPClient(RequestTraversalAgent(Root(reactor)))
+        response = yield treq.get('http://127.0.0.1:8888/')
+        print(response)
+        assert False
