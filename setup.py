4232a4f78a00fbf70c554d8595b7ba73d3fde9e0
Tom Most <twm@freecog.net>
2018-07-17 22:30:36 -0700

    Require Twisted[http2] for great SPDY

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     install_requires=[
         'attrs == 18.1.0',
         'Django ~= 2.0.1',
-        'Twisted[tls] == 18.7.0',
+        'Twisted[tls,http2] == 18.7.0',
         'treq >= 17.8.0',
         'pytz',
         # We are (hopefully temporarily) using a fork of feedparser as the
