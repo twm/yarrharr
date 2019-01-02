bd853f3ffb19953e858f716d369e1cdde1f4529d
Tom Most <twm@freecog.net>
2019-01-01 19:30:35 -0800

    Seems to work, less CSS and custom plugins

--- webpack.config.js
+++ webpack.config.js
@@ -2,63 +2,75 @@ const webpack = require('webpack');
 const path = require('path');
 
 const config = {
-  entry: './assets/entry.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.svg$/,
-        use: 'file-loader'
-      },
-      {
-        test: /\.less$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'less-loader'
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }
-    ]
-  },
-  resolve: {
-    extensions: [
-      '.js',
-      '.jsm'
-    ]
-  },
-  devServer: {
-    contentBase: './dist' // FIXME
-  },
-  plugins: [
-      // TODO
-  ],
-  optimization: {
-    runtimeChunk: 'single',
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\\/]node_modules[\\\/]/,
-          name: 'vendors',
-          chunks: 'all'
-        }
-      }
-    }
-  }
+    entry: './assets/entry.js',
+    resolve: {
+        modules: [
+            path.join(__dirname, 'assets'),
+            'node_modules',
+        ],
+        extensions: [
+            '.js',
+            '.jsm',
+        ],
+    },
+    module: {
+        rules: [{
+        // FIXME WP4: Restore eslint
+            // test: /\.jsm?$/,
+            // enforce: 'pre',
+            // loaders: ['eslint-loader'],
+            // exclude: /node_modules/,
+        // }, {
+            test: /\.less$/,
+            use: [{
+                loader: 'style-loader', // FIXME WP4: Restore separate CSS file
+            }, {
+                loader: 'css-loader',
+                // FIXME WP4
+                // options: {minimize: production},
+            }, {
+                loader: 'less-loader',
+                options: {strictMath: true, noIeCompat: true},
+            }],
+        }, {
+            test: /\.jsm?$/,
+            loaders: ['babel-loader'],
+            exclude: /node_modules/,
+        }, {
+            // Files which must currently be used as images.
+            // icon.svg — included as a file from non-React pages.
+            test: /assets\/art\/icon\.svg$/,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    name: '[name]-[hash].[ext]',
+                },
+            }],
+        }],
+    },
+    output: {
+        path: path.resolve(__dirname, 'yarrharr/static'),
+        filename: '[name]-[contenthash].js',
+    },
+    plugins: [
+        // TODO
+    ],
+    devtool: "source-map",
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\\/]node_modules[\\\/]/,
+                    name: 'vendor',
+                    chunks: 'all'
+                },
+            },
+        },
+    },
+    devServer: {
+        contentBase: './dist' // FIXME
+    },
 }
 
 module.exports = config;
