8579b166bcb61300b14f2583f07aaa4a1dcca9c3
Tom Most <twm@freecog.net>
2019-06-23 23:25:40 -0700

    Anoint 2019.6.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if sys.version_info.major == 2:
 
 setup(
     name='yarrharr',
-    version='2019.6.0',
+    version='2019.6.1',
     url='https://github.com/twm/yarrharr',
     author='Tom Most',
     author_email='yarrharr@freecog.net',
