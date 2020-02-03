a75ac5c8d534cb0b1e86fa1d3ce08595d37a203e
Tom Most <twm@freecog.net>
2020-02-02 23:10:28 -0800

    Anoint 2020.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if sys.version_info.major == 2:
 
 setup(
     name='yarrharr',
-    version='2020.1.1',
+    version='2020.2.0',
     url='https://github.com/twm/yarrharr',
     author='Tom Most',
     author_email='yarrharr@freecog.net',
