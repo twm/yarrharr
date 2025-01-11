2ab7fc56f16e5b607eb12a6103a4494a1b517fc3
Tom Most <twm@freecog.net>
2025-01-11 12:28:25 -0800

    Smells like subtests

--- yarrharr/tests/test_views.py
+++ yarrharr/tests/test_views.py
@@ -121,19 +121,17 @@ class LoginRedirectTests(TestCase):
             "/article/1234/",
         ]
 
-        redirects = []
         for next_ in nexts:
-            response = c.post(
-                "/login/",
-                {
-                    "next": next_,
-                    "username": "james",
-                    "password": "hunter2",
-                },
-            )
-            redirects.append(response["Location"])
-
-        self.assertEqual(nexts, redirects)
+            with self.subTest("login", next=next_):
+                response = c.post(
+                    "/login/",
+                    {
+                        "next": next_,
+                        "username": "james",
+                        "password": "hunter2",
+                    },
+                )
+                self.assertEqual(next_, response["Location"])
 
 
 class LabelListTests(TestCase):
