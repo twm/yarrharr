2ab1d350b337fc3386f2da92a16983f6fbdbcc74
Tom Most <twm@freecog.net>
2026-07-01 19:29:57 -0700

    Anoint 2026.7.0

--- yarrharr/_version.py
+++ yarrharr/_version.py
@@ -7,5 +7,5 @@ Provides yarrharr version information.
 
 from incremental import Version
 
-__version__ = Version("yarrharr", 2026, 6, 1)
+__version__ = Version("yarrharr", 2026, 7, 0)
 __all__ = ["__version__"]
